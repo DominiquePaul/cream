da619df1242a886281c111f484f689f8ef68f7c4
Dominique Paul <dominique.c.a.paul@gmail.com>
2025-03-31 18:26:24 +0200

    Added overlay to wait while server is loading

---
+++ cream_modal/src/constants.py
@@ -0,0 +1,7 @@
+# Constants for the DreamStream application
+
+# Default mount path for model volumes
+MODEL_MOUNT_PATH = "/app/modal_models"
+
+# Default style prompt
+DEFAULT_STYLE_PROMPT = "A painting in the style of van Gogh's 'Starry Night'" 
